123d1829c098177766ea10aed1f75f303df0d782
Telefoniphone <stanislav888888888888@gmail.com>
2025-03-11 00:31:22 +0300

    Delete components/ui directory

--- components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Card({ children }: { children: React.ReactNode }) {
-  return <div className="p-4 shadow-md rounded-lg bg-white">{children}</div>;
-}
-
-export function CardContent({ children }: { children: React.ReactNode }) {
-  return <div className="p-2">{children}</div>;
-}
