06d41f888f53c0ab86ce2f86c0da214795acf2d4
Telefoniphone <stanislav888888888888@gmail.com>
2025-03-11 00:43:19 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['yourdomain.com'], // Замените 'yourdomain.com' на ваш домен
+  },
+  // Другие настройки Next.js могут быть добавлены здесь
+}
+
+module.exports = nextConfig
