bfc2fee5470c9a48933b0dc995e6ba9faec4c148
StanislavBorisov897 <stanislav888888888888@gmail.com>
2025-03-12 10:30:04 +0300

    Initial commit

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
-=======
->>>>>>> bb10e9a (Fix type error by extending Session and Token types)
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
-<<<<<<< HEAD
-=======
->>>>>>> d503a63 (Initial commit)
->>>>>>> bb10e9a (Fix type error by extending Session and Token types)
--- pages/api/auth/next-auth.ts
+++ pages/api/auth/next-auth.ts
@@ -1,38 +1,15 @@
-import NextAuth from "next-auth";
-<<<<<<< HEAD
-import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions } from "next-auth";
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions: NextAuthOptions = {
+const options = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  callbacks: {
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
-  pages: {
-    signIn: '/auth/signin',
-    signOut: '/auth/signout',
-    error: '/auth/error', // Error code passed in query string as ?error=
-  },
 };
 
-export default NextAuth(authOptions);
-=======
-
-declare module "next-auth" {
-  interface Session {
-    accessToken?: string;
-  }
-  interface Token {
-    accessToken?: string;
-  }
-}
->>>>>>> bb10e9a (Fix type error by extending Session and Token types)
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
